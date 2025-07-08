f39173a76e90cae1e4f29fcab7fbcfa021e1e9f8
Ivan Trajanovski <ivantrajanovski@gmail.com>
2025-07-08 11:46:38 +0200

    shake not a bug dialog

--- lib/features/library/view/widgets/not_bug_dialog.dart
+++ lib/features/library/view/widgets/not_bug_dialog.dart
@@ -1,77 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
 
-class NotBugDialog extends StatelessWidget {
+class NotBugDialog extends StatefulWidget {
   const NotBugDialog({super.key});
+
+  @override
+  State<NotBugDialog> createState() => _NotBugDialogState();
+}
+
+class _NotBugDialogState extends State<NotBugDialog> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _shakeAnim;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+    _shakeAnim = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: -16.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: -16.0, end: 14.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 14.0, end: -12.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: -12.0, end: 10.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 10.0, end: -8.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: -8.0, end: 6.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 6.0, end: 0.0), weight: 1),
+    ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-
-    return AlertDialog(
-      backgroundColor: isDarkMode ? const Color(0xFF2A2A36) : Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      title: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.orange.withValues(alpha: 0.1),
-              borderRadius: BorderRadius.circular(12),
+    return AnimatedBuilder(
+      animation: _shakeAnim,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(_shakeAnim.value, 0),
+          child: child,
+        );
+      },
+      child: AlertDialog(
+        backgroundColor: isDarkMode ? const Color(0xFF2A2A36) : Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        title: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.orange.withValues(alpha: 0.1),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(
+                HugeIcons.strokeRoundedAlertCircle,
+                color: Colors.orange,
+                size: 24,
+              ),
             ),
-            child: Icon(
-              HugeIcons.strokeRoundedAlertCircle,
-              color: Colors.orange,
-              size: 24,
+            const SizedBox(width: 12),
+            const Expanded(
+              child: Text(
+                'No Bug Detected',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
             ),
-          ),
-          const SizedBox(width: 12),
-          const Expanded(
-            child: Text(
-              'No Bug Detected',
+          ],
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'The AI couldn\'t identify a bug in this image. This could be because:',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 12),
+            _buildTip('The image doesn\'t contain a bug or insect'),
+            _buildTip('The bug is too small or blurry to identify'),
+            _buildTip('The lighting or angle makes it difficult to see'),
+            const SizedBox(height: 12),
+            const Text(
+              'Try taking a clearer photo of the bug from a closer distance.',
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+                fontSize: 14,
+                fontStyle: FontStyle.italic,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text(
+              'Try Again',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
               ),
             ),
           ),
         ],
       ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'The AI couldn\'t identify a bug in this image. This could be because:',
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 12),
-          _buildTip('The image doesn\'t contain a bug or insect'),
-          _buildTip('The bug is too small or blurry to identify'),
-          _buildTip('The lighting or angle makes it difficult to see'),
-          const SizedBox(height: 12),
-          const Text(
-            'Try taking a clearer photo of the bug from a closer distance.',
-            style: TextStyle(
-              fontSize: 14,
-              fontStyle: FontStyle.italic,
-              color: Colors.grey,
-            ),
-          ),
-        ],
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text(
-            'Try Again',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 16,
-            ),
-          ),
-        ),
-      ],
     );
   }
 
